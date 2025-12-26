f9229dfee4c54e7351ea161d386cb30178c97525
Duck <36255460+Duckuk@users.noreply.github.com>
2025-12-26 21:01:48 +1030

    Hash table definitions

---
+++ include/hash_table.h
@@ -0,0 +1,11 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <assert.h>
+#include <stddef.h>
+
+size_t hash(void *restrict buf, size_t buf_size) { assert(0); }
+
+struct hash_table {};
+
+#endif
