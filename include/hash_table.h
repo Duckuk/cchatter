531f755ebd07ba44ae7e242d4a1a896e149c6487
Duck <36255460+Duckuk@users.noreply.github.com>
2025-12-30 22:13:25 +1030

    Add periods to hash_table documentation

--- include/hash_table.h
+++ include/hash_table.h
@@ -20,24 +20,24 @@ struct hash_table {
   size_t (*_hash_func)(void *restrict, size_t);
 };
 
-// Generate a hash from variable-length buf using homemade block-xoring
+// Generate a hash from variable-length buf using homemade block-xoring.
 size_t hash(void *restrict buf, size_t buf_size);
 
 // Randomly generate a bunch of data and hash it.
 double calculate_collision_rate();
 
-// Initialize new hash table with default capacity (1024)
+// Initialize new hash table with default capacity (1024).
 // Return 0 on success, otherwise returns -1
 int hash_table_new(struct hash_table *restrict table, size_t key_size,
                    size_t element_size);
-// Set `key` to `element` in `table`
+// Set `key` to `element` in `table`.
 // Return 0 on success, otherwise returns -1
 int hash_table_set(struct hash_table *restrict table, void *restrict key,
                    void *restrict element);
-// Delete `key` from `table`
+// Delete `key` from `table`.
 // Return 0 on success, otherwise returns -1
 int hash_table_remove(struct hash_table *restrict table, void *restrict key);
-// Get `key` from `table`
+// Get `key` from `table`.
 // Return 0 on success, otherwise returns -1
 void *hash_table_get(struct hash_table *restrict table, void *restrict key);
 
